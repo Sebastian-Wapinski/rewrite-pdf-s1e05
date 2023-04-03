5108ec35b76d1e08c5e6ffe3603ef003b9ba5c14
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-03 19:23:55 +0200

    dataset

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const boxList = document.getElementsByClassName('box');
-for (let i = 0; i < boxList.length; i++) {
-    boxList[i].textContent = 'index -> ' + i;
+const liElement = document.querySelector('li');
+if (liElement) {
+    console.log(
+        liElement.dataset.age,
+        liElement.dataset.city,
+    );
 }
-
-const arr = Array.from(boxList);
-arr.forEach(function(element) {
-    element.style.border = '1px solid green'
-})
