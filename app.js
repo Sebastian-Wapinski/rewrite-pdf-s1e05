b9586feccb71ca25da7fedccc56ca00b3de41a5d
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-03 17:29:35 +0200

    querySelector

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const fooEl = document.querySelector('#foo');
-if (fooEl) {
-    const boxList = fooEl.getElementsByClassName('box');
-    for (let i = 0; i < boxList.length; i++) {
-        const pList = boxList[i].querySelectorAll('p');
-        pList.forEach(function(p) {
-            p.style.border = '2px solid green';
-        });
-    }
-}
+const sectionElement = document.querySelector('section');
+const headerElementInSection = sectionElement.querySelector('header');
